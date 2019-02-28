dec3746f7652e11ed9e9caab5f3ac7ba3b0017ea
Abhishek C.N <42774788+AbhishekCN1994@users.noreply.github.com>
2019-02-28 09:18:44 +0530

    Update Factorial.c

--- Factorial.c
+++ Factorial.c
@@ -3,7 +3,8 @@
 void main(){
  unsigned long long int factorial = 1;
  int n = 5;
- for(int i = 1; i <= n; i ++){
+ int i;
+ for(i = 1; i <= n; i ++){
 	factorial *= i;
  }
  printf("%llu",factorial);
